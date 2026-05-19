0da5a3be5e90a2811b0b6655594d011501df00d4
mello0010 <mellofellipe12@gmail.com>
2026-05-19 11:31:06 -0300

    ex01

---
+++ ex1/script.js
@@ -0,0 +1,13 @@
+const alunos = [
+    
+{nome:"Vitor", idade:16, DisciplinaPreferida:"EducacaoFisica"},
+{nome:"Giovanne", idade:16, DisciplinaPreferida:"Biologia"},
+{nome:"Caua", idade:16, DisciplinaPreferida:"EducacaoFisica"},
+{nome:"Ligya", idade:16, DisciplinaPreferida:"Biologia"},
+{nome:"Joao", idade:16, DisciplinaPreferida:"EducacaoFisica"},
+];
+alert (`o nome do primeiro aluno é : ${alunos[0].nome}`)
+alert (`a materia preferida do ultimo aluno é : ${alunos[4].DisciplinaPreferida
+    }`)
+
+
